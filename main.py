55c0e4ce806d43f84703734079ebdaeba7937d03
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-08 08:24:44 +0530

    add img and frame and label create

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ root.geometry('925x500+300+200')
 root.configure(bg="#fff")
 root.resizable(False,False)
 
+img = PhotoImage(file='login.png')
+Label(root,image=img,bg='white').place(x=50, y=50)
+
+frame = Frame(root, width=350, height=350, bg="white")
+frame.place(x=480, y=70)
+
+heading = Label(frame, text='Sign in', fg='#57a1f8', bg='white', font=('Microsoft YaHei UI Light', 23, 'bold'))
+heading.place(x=100, y=5)
+
 root.mainloop()
