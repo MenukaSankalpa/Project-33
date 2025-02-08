c7e411cb7ac94ee88c0265400bc9328d6221e18a
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-08 08:46:36 +0530

    username and password box created

--- main.py
+++ main.py
@@ -16,4 +16,17 @@ frame.place(x=480, y=70)
 heading = Label(frame, text='Sign in', fg='#57a1f8', bg='white', font=('Microsoft YaHei UI Light', 23, 'bold'))
 heading.place(x=100, y=5)
 
+##################
+user = Entry(frame,width=25, fg='black', border=0, bg="white", font=('Microsoft YaHei UI Light', 11))
+user.place(x=30, y=80)
+user.insert(0, 'Username')
+
+Frame(frame,width=295, height=2, bg='black').place(x=25,y=107)
+
+##################
+code = Entry(frame,width=25, fg='black', border=0, bg="white", font=('Microsoft YaHei UI Light', 11))
+code.place(x=30, y=150)
+code.insert(0, 'Password')
+
+Frame(frame,width=295, height=2, bg='black').place(x=25,y=177)
 root.mainloop()
