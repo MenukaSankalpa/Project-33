44c7c7d7c5e1a4fe0e70859fb3550688cef7e4dc
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-08 09:00:17 +0530

    create button

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ heading.place(x=100, y=5)
 user = Entry(frame,width=25, fg='black', border=0, bg="white", font=('Microsoft YaHei UI Light', 11))
 user.place(x=30, y=80)
 user.insert(0, 'Username')
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
 
 Frame(frame,width=295, height=2, bg='black').place(x=25,y=107)
 
@@ -28,5 +30,17 @@ code = Entry(frame,width=25, fg='black', border=0, bg="white", font=('Microsoft 
 code.place(x=30, y=150)
 code.insert(0, 'Password')
 
+
 Frame(frame,width=295, height=2, bg='black').place(x=25,y=177)
+
+##################
+Button(frame,width=39,pady=7, text='Sign in', bg='#57a1f8', fg='white',border=0).place(x=35, y=204)
+label = Label(frame,text="Dont have an account? ", fg='black', bg='white', font=('Microsoft YaHei UI Light', 9))
+label.place(x=75, y=270)
+
+sign_up = Button(frame,width=6, text='Sign Up', border=0, bg='white', cursor='hand2', fg='#57a1f8')
+sign_up.place(x=215, y=270)
+
+
+
 root.mainloop()
