cfec8e2e32f01e94b3f7b6272e43fba8ad4e106d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-09 09:35:10 +0530

    create signup window

---
+++ main2.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+import ast
+
+window = Tk()
+window.title("SingUp")
+window.geometry('925x550+300+200')
+window.configure(bg="#fff")
+window.resizable(False, False)
+
+
+window.mainloop()
