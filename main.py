a3f6d0b278ede9f32b60b8bebbf168f5aa07d6b7
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-09 13:26:14 +0530

    complete to shift windows

--- main.py
+++ main.py
@@ -30,15 +30,131 @@ def signin():
         
         screen.mainloop()
     
-    elif username !='admin' and password != '1234':
+    else:
+        messagebox.showerror('Invalid', 'invalid username or password')
+        
+    '''elif username !='admin' and password != '1234':
         messagebox.showerror("Invalid", "Invalid username or password")
         
     elif password != "1234":
         messagebox.showerror("Invalid", "Invalid password")
     
     elif username != 'admin':
-        messagebox.showerror("Invalid", "Invalid Username")
+        messagebox.showerror("Invalid", "Invalid Username")  '''  
+###---
+def signup_command():
+    window = Toplevel(root)
+    window.title("SingUp")
+    window.geometry('925x550+300+200')
+    window.configure(bg="#fff")
+    window.resizable(False, False)
+    
 
+
+    def signup():
+        username = user.get()
+        password = code.get()
+        conform_password = conform_code.get()
+        
+        if password == conform_password:
+            try:
+                file = open('datasheet.txt', 'r+')
+                d = file.read()
+                r = ast.literal_eval(d)
+                
+                dict2={username:password}
+                r.update(dict2)
+                file.truncate(0)
+                file.close()
+                
+                file = open('datasheet.txt', 'w')
+                w = file.write(str(r))
+                
+                messagebox.showinfo('Signup', 'Successfully sign up')
+                
+            except:
+                file = open('datasheet.txt', 'w')
+                pp = str({'username':'password'})
+                file.write(pp)
+                file.close()
+                
+        else:
+            messagebox.showerror('Invalid', "Both Password should match")        
+            
+    def sign():
+        window.destroy()
+    
+    
+    img = PhotoImage(file='login2.png')
+    Label(window, image=img, bg='white').place(x=50, y=90)
+    
+    frame = Frame(window, width=350, height=390, bg='#fff')
+    frame.place(x=480, y=50)
+    
+    heading = Label(frame, text='Sing Up', fg='#57a1f8', bg='white', font=('Microsoft Yahei UI Light', 23, 'bold'))
+    heading.place(x=100, y=5)
+
+###---k
+    def on_enter(e):
+        code.delete(0, 'end')
+    
+    def on_leave(e):
+        if code.get()=='':
+            code.insert(0, 'Password') 
+    
+    code = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft Yahei UI Light', 11))
+    code.place(x=30, y=150)
+    code.insert(0, 'Password')
+    code.bind("<FocusIn>", on_enter)
+    code.bind("FocusOut", on_leave)
+    
+    Frame(frame, width=295, height=2, bg='black').place(x=25, y=177)
+
+###---
+    def on_enter(e):
+        user.delete(0, 'end')
+    
+    def on_leave(e):
+        if user.get()=='':
+            user.insert(0, 'Username') 
+    
+    user = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft Yahei UI Light', 11))
+    user.place(x=30, y=80)
+    user.insert(0, 'username')
+    user.bind("<FocusIn>", on_enter)
+    user.bind("FocusOut", on_leave)
+
+    Frame(frame, width=295, height=2, bg='black').place(x=25, y=107)
+
+###---
+    def on_enter(e):
+        conform_code.delete(0, 'end')
+
+    def on_leave(e):
+        if conform_code.get()=='':
+            conform_code.insert(0, 'Conform Password') 
+
+    conform_code = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft Yahei UI Light', 11))
+    conform_code.place(x=30, y=220)
+    conform_code.insert(0, 'Conform Password')
+    conform_code.bind("<FocusIn>", on_enter)
+    conform_code.bind("FocusOut", on_leave)
+
+    Frame(frame, width=295, height=2, bg='black').place(x=25, y=247)
+
+###---
+
+    Button(frame, width=39, pady=7, text='Sign up', bg='#57a1f8', fg='white', border=0, command=signup).place(x=35, y=280)
+    label = Label(frame, text='I have an account', fg='black', bg='white', font=('Microsoft Yahei UI light', 9))
+    label.place(x=90, y=340)
+
+    signin = Button(frame, width=6, text='Sign in', border=0, bg='white', cursor='hand2', fg='#57a1f8', command=sign)
+    signin.place(x=200, y=340)
+
+
+    window.mainloop()    
+
+#####-----
 img = PhotoImage(file='login.png')
 Label(root,image=img,bg='white').place(x=50, y=50)
 
@@ -90,7 +206,7 @@ Button(frame,width=39,pady=7, text='Sign in', bg='#57a1f8', fg='white',border=0,
 label = Label(frame,text="Dont have an account? ", fg='black', bg='white', font=('Microsoft YaHei UI Light', 9))
 label.place(x=75, y=270)
 
-sign_up = Button(frame,width=6, text='Sign Up', border=0, bg='white', cursor='hand2', fg='#57a1f8')
+sign_up = Button(frame,width=6, text='Sign Up', border=0, bg='white', cursor='hand2', fg='#57a1f8', command=signup_command)
 sign_up.place(x=215, y=270)
 
 
